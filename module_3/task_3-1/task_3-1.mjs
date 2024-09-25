a360e642365b64b9d4588e735aa9db43bc1019d6
NatalieSol <nataliess@uia.no>
2024-09-25 15:26:03 +0200

    Ferdig med task 2.2

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -3,11 +3,18 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Complete the given "if" in the task_3-1.mjs file at part 1, statement so that it matches this: If I wake up at
+exactly 7 o'clock then I can catch the bus to school. Run the program with different values of wake-up time
+(6, 7, 8).*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+
+let wakeUpTime = 7;
+const haveToWakeUp = 7;
+if (wakeUpTime <= haveToWakeUp){
+  printOut( "If i wake up at exactly " + haveToWakeUp + ", i can take the bus to school" );
+  
+}else { printOut( " I cannot make the bus, because i woke up at " + wakeUpTime + ", the bus leaves at" + haveToWakeUp )
+
 }
 printOut(newLine);
 
