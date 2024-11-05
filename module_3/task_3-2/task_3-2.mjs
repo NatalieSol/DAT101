3a4e83fdb842335e97f8993ff4672a6882d2b1ca
NatalieSol <nataliess@uia.no>
2024-11-05 12:25:15 +0100

    Ferdig med task.3.2

--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -3,33 +3,232 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Use "for" loops to generate two lines on the HTML page. One should count from 1 to 10, and the other
+should count from 10 to 1. Use only two lines to print the rows.*/
+
+let txt = " ";
+const numberTen = 10;
+for (let oneToTen = 1; oneToTen<= numberTen; oneToTen++) {
+    txt += " " + oneToTen.toString();
+}
+printOut(txt)
+txt = "";
+const numberOne = 1;
+for (let tenToOne = 10; tenToOne>= numberOne; tenToOne--){
+    txt += " " + tenToOne.toString();
+}
+printOut(txt);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create a program that can guess a number between 1 and 60. Declare a variable and assign it a value, for
+example, 45. Let the computer "guess" by generating a random number. Use a "while" loop and the
+"random" function. Keep the "while" loop running as long as the "guessed number" is incorrect. Print the
+number once the "while" loop has completed. You do not need to print anything while the "while" loop is in
+progress.*/
+
+const nrToGuess = 45;
+let rndNr = 0;
+while(nrToGuess !== rndNr){
+    rndNr = Math.ceil(Math.random() * 60);
+}
+printOut("Guess number= " + rndNr.toString());
+printOut(" ");
+
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Take the program from part 2 and expand it to guess a number between 1 and one million. Print the
+number of guesses as well as the number of milliseconds it took to guess the number. HINT: Use the
+Date.now() function to measure time*/
+
+rndNr = 0;
+let guessCount = 0;
+const startTime = Date.now();
+while(nrToGuess !== rndNr){
+    guessCount++;
+    rndNr = Math.ceil(Math.random() * 1000000);
+}
+const endTime = Date.now();
+const timeUsed = endTime - startTime;
+printOut("Antall gjetninger: " + guessCount.toString() + ". Det tok: " + timeUsed.toString() + " ms");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Use a "for" loop and a "while" loop to find all prime numbers greater than 1 and less than 200.
+○ HINT: A prime number is any natural number greater than 1 that is only divisible by itself and
+1. The number 1 is not a prime. (See Wikipedia on primes or ask your AI).*/
+
+for (let numberTwo = 2; numberTwo <= 200; numberTwo ++) {
+    let notPrime = false;
+    let divide = 2;
+    while ((notPrime === false) && (divide < numberTwo)) {
+        notPrime = (numberTwo % divide) === 0;
+        divide++;
+    }
+    if (notPrime === false) {
+      printOut(numberTwo.toString() + " ");
+    }
+}
+
+
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create two loops that print 9 columns and 7 rows with the text "K1, R1" for the first cell, "K2, R1" for the
+second cell, and so on.
+○ Hint: Use what we call nested loops. This is a "for" loop within another "for" loop.
+Use the provided printOut function to print each row with its sets of columns; remember to place this in
+the right level of the nested for loops. The output should look like this:
+K1R1 K2R1 K3R1 K4R1 K5R1 K6R1 K7R1 K8R1 K9R1
+K1R2 K2R2 K3R2 K4R2 K5R2 K6R2 K7R2 K8R2 K9R2
+K1R3 K2R3 K3R3 K4R3 K5R3 K6R3 K7R3 K8R3 K9R3
+K1R4 K2R4 K3R4 K4R4 K5R4 K6R4 K7R4 K8R4 K9R4
+K1R5 K2R5 K3R5 K4R5 K5R5 K6R5 K7R5 K8R5 K9R5
+K1R6 K2R6 K3R6 K4R6 K5R6 K6R6 K7R6 K8R6 K9R6
+K1R7 K2R7 K3R7 K4R7 K5R7 K6R7 K7R7 K8R7 K9R7*/
+
+const rows = 10;
+const columns = 8;
+let rowText = "";
+let colText = "";
+let wholeText = "";
+for (let j = 1; j < columns; j++){
+    wholeText += newLine
+    rowText = "R" + j;
+for (let i = 1; i < rows; i++){
+    colText = "K" + i;
+ wholeText +=(colText + rowText);
+    }
+   
+}
+
+
+printOut(wholeText);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Simulate 5 student grades using the Math.random() function, from 1 to 236 (inclusive).
+For each grade, print the student's grade (A to F) based on the point distribution provided:
+○ A: 89% – 100%
+○ B: 77% – 88%
+○ C: 65% – 76%
+○ D: 53% – 64%
+○ E: 41% – 52%
+○ F: 0% – 40%
+Sorting Challenge (Bonus): Sort and print the 5 grades in descending order (from A to F) without using
+an array. You can use a for loop and a do/while loop to achieve this.
+Hint for Success: If you successfully complete the sorting challenge, you'll unlock a valuable hint for Part
+9 of "DAT101: Mandatory assignment 4.1" that will make it easier to solve. The learning outcomes remain
+the same, but this hint will give you a head start!*/
+
+const maxGrade = 236;
+
+let grade1 = Math.ceil(Math.random() * maxGrade);
+let grade2 = Math.ceil(Math.random() * maxGrade);
+let grade3 = Math.ceil(Math.random() * maxGrade);
+let grade4 = Math.ceil(Math.random() * maxGrade);
+let grade5 = Math.ceil(Math.random() * maxGrade);
+
+let prc1 = (grade1 / maxGrade) * 100;
+let prc2 = (grade2 / maxGrade) * 100;
+let prc3 = (grade3 / maxGrade) * 100;
+let prc4 = (grade4 / maxGrade) * 100;
+let prc5 = (grade5 / maxGrade) * 100;
+
+let pc1 = Math.ceil(prc1);
+let pc2 = Math.ceil(prc2);
+let pc3 = Math.ceil(prc3);
+let pc4 = Math.ceil(prc4);
+let pc5 = Math.ceil(prc5);
+
+const GradeF = 40;
+const GradeE = 52;
+const GradeD = 64;
+const GradeC = 76;
+const GradeB = 88;
+const GradeA = 100;
+
+printOut("Student 1 point: " + pc1);
+
+if(pc1 <= GradeF){
+    printOut("You got an F");
+}else if(pc1 <= GradeE){
+    printOut("You got an E");
+}else if(pc1 <= GradeD){
+    printOut("You got a D");
+}else if(pc1 <= GradeC){
+    printOut("You got a C");
+}else if(pc1 <= GradeB){
+    printOut("You got a B");
+}else if(pc1 <= GradeA){
+    printOut("You got an A!");
+}
+printOut(newLine);
+printOut("Student 2 points: " + pc2);
+
+if(pc2 <= GradeF){
+    printOut("You got an F");
+}else if(pc2 <= GradeE){
+    printOut("You got an E");
+}else if(pc2 <= GradeD){
+    printOut("You got a D");
+}else if(pc2 <= GradeC){
+    printOut("You got a C");
+}else if(pc2 <= GradeB){
+    printOut("You got a B");
+}else if(pc2 <= GradeA){
+    printOut("You got an A!");
+}
+printOut(newLine);
+printOut("Student 3 points: " + pc3);
+
+if(pc3 <= GradeF){
+    printOut("You got an F");
+}else if(pc3 <= GradeE){
+    printOut("You got an E");
+}else if(pc3 <= GradeD){
+    printOut("You got a D");
+}else if(pc3 <= GradeC){
+    printOut("You got a C");
+}else if(pc3 <= GradeB){
+    printOut("You got a B");
+}else if(pc3 <= GradeA){
+    printOut("You got an A!");
+}
+printOut(newLine);
+printOut("Student 4 grade: " + pc4);
+
+if(pc4 <= GradeF){
+    printOut("You got an F");
+}else if(pc4 <= GradeE){
+    printOut("You got an E");
+}else if(pc4 <= GradeD){
+    printOut("You got a D");
+}else if(pc4 <= GradeC){
+    printOut("You got a C");
+}else if(pc4 <= GradeB){
+    printOut("You got a B");
+}else if(pc4 <= GradeA){
+    printOut("You got an A!");
+}
+printOut(newLine);
+printOut("Student 5 grade: " + pc5);
+
+if(pc5 <= GradeF){
+    printOut("You got an F");
+}else if(pc5 <= GradeE){
+    printOut("You got an E");
+}else if(pc5 <= GradeD){
+    printOut("You got a D");
+}else if(pc5 <= GradeC){
+    printOut("You got a C");
+}else if(pc5 <= GradeB){
+    printOut("You got a B");
+}else if(pc5 <= GradeA){
+    printOut("You got an A!");
+}
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
@@ -94,18 +293,3 @@ if(equals1 === 6){
 }
 printOut(newLine);
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-/* Task 10*/
-printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
--- module_3/task_3-3/task_3-3.mjs
+++ module_3/task_3-3/task_3-3.mjs
@@ -3,52 +3,105 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create a function that takes no parameters and returns no values. Have it print today's date in the
+Norwegian standard. Example: "Friday, October 18, 2019" Use an example from this resource:
+toLocaleString , Use "no-NB" as an alias for the Norwegian language in the function call to
+"toLocaleDateString".*/
+const d = new Date();
+const option = {
+    weekday: "long",
+    month: "long",
+    year: "numeric",
+    day: "numeric"
+}
+let text = d.toLocaleDateString('no-NO', option);
+
+
+printOut( text );
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Level Up Your Date Function: Take your "Today's Date" function from Task 1 and supercharge it! Not
+only should it display today's date in elegant Norwegian fashion, but it also needs to return that date as a
+powerful date object, ready for further manipulation.
+The Hype Train is Leaving the Station: Craft a new function that calculates the number of days left until
+the epic release of 2XKO, the highly-anticipated tag-team fighting game set in the League of Legends
+universe, launching on May 14th, 2025.
+Time for the Grand Reveal: Combine the might of your two functions to print today's date and the thrilling
+countdown to 2XKO's debut. Feel free to add a bit of flair to your output - maybe a themed message or a
+touch of visual excitement!
+Remember:
+● This task isn't just about coding; it's about harnessing the power of dates and functions to create
+something both informative and engaging.
+● Accuracy is key! Make sure your countdown is precise and your date formatting is impeccable.
+● Creativity is encouraged! Let your passion for gaming and multimedia shine through in your output.*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a function that receives the radius of a circle and prints the diameter, circumference, and area.*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a function that receives the width and height of a rectangle in an object. Print the circumference
+and area of the given rectangle.*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a function that handles the conversion between Celsius, Fahrenheit, and Kelvin. Use three different
+numbers and print all three combinations as integers (no decimals). Design the function to take two
+parameters: first the temperature, then the temperature type/id. Use these parameters to convert to the
+other two temperature types and print them. Formula:
+*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a function that calculates the price without VAT (sales tax). The function needs two arguments, one
+for the price including VAT (gross amount) and one for the tax group in text (normal = 25%, food = 15%,
+hotel, transport, and cinema = 10%). The text argument should not be case-sensitive. If the VAT group is
+not correct, the text "Unknown VAT group!" should be printed. The function must return the price without
+tax, i.e., the net price. Call the function four times with different gross amounts. One for each of the VAT
+groups (25, 15, and 10) and one with an unknown group for example “goblins”. Tip: Use "NaN" to identify
+that an unknown VAT group is returned from the function. Formula: net = (100 * gross) / (vat + 100).*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a function that takes 3 arguments and returns the following calculation:
+● Speed = Distance / Time
+If speed is missing, calculate speed. If time is missing, calculate time. If distance is missing, calculate the
+distance. If more than one parameter is missing, return NaN.*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Create a function that takes four parameters and returns a result. Parameter one: A text string. Parameter
+two: Value for the maximum size of the text string. Parameter three: Text character. Parameter four:
+Consecutive insertion of characters (boolean value). Take the text parameter; if it's smaller than the
+maximum, make it larger with the specified character, either before or after, using the given boolean value.
+Have the function return the new string and print it out.*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* From mathematics, we have the following expression:
+1 + 2 = 3
+4 + 5 + 6 = 7 + 8
+9 + 10 + 11 + 12 = 13 + 14 + 15
+16 + 17 + 18 + 19 + 20 = 21 + 22 + 23 + 24
+25 + 26 + 27 + 28 + 29 + 30 = 31 + 32 + 33 + 34 + 35
+Create a function or functions that can test this expression for 200 lines. If the test fails, print out where the
+two sides are not equal and stop the loop. If all 200 lines are OK, print "Maths fun!".*/
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Recursive function. Create a function that calculates the factorial of a given number. Factorial of 5 = 5 * 4 *
+3 * 2 * 1. Factorial of 6 = 6 * 5 * 4 * 3 * 2 * 1. Etc.
+Have the function call itself to calculate the result and print the final answer.*/
 printOut("Replace this with you answer!");
 printOut(newLine);
